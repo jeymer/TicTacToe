a8354807ad8fb8edab33def449d47cb6cbdddb92
Jeffrey <jeymer@live.com>
2017-01-11 16:51:59 -0500

    Added method to change text color of squares

--- Tic Tac Toe/src/tictactoe/Grid.java
+++ Tic Tac Toe/src/tictactoe/Grid.java
@@ -4,27 +4,29 @@
 
 package tictactoe;
 
+import java.awt.Color;
+
 public class Grid {
-	
+
 	public MainWindow mainWindow; // Used to pass along to the square constructor
 	public Square[][] grid = new Square[3][3]; // 3x3 grid of Square objects
-	
+
 	public Grid(MainWindow mainWindow) {
 		this.mainWindow = mainWindow;
 		for(int r = 0; r < 3; r++) {
 			for(int c = 0; c < 3; c++) {
 				grid[r][c] = new Square(mainWindow, r, c);
-				
+
 			}
 		}
 	}
-	
+
 	// Get square at (x, y)
 	public Square getSquare(int xCoord, int yCoord) {
 		return this.grid[xCoord][yCoord];
 	}
-	
-	
+
+
 	// Delegate method to clear all squares
 	public void clear() {
 		for(int r = 0; r < 3; r++) {
@@ -34,8 +36,18 @@ public class Grid {
 		}
 	}
 	
-public int checkWinner() {
-		
+	// Method to change color of the grid squares(buttons)
+	public void setColor(Color color) {
+		for(int r = 0; r < 3; r++) {
+			for(int c = 0; c < 3; c++) {
+				this.grid[r][c].button.setForeground(color);
+			}
+		}
+	}
+
+
+	public int checkWinner() {
+
 		// Check rows for winner
 		for(int r = 0; r < 3; r++) {
 			if(grid[r][0].value != 0 
@@ -45,7 +57,7 @@ public int checkWinner() {
 				return 2;
 			}
 		}
-		
+
 		// Check columns for winner
 		for(int c = 0; c < 3; c++) {
 			if(grid[0][c].value != 0 
@@ -55,7 +67,7 @@ public int checkWinner() {
 				return 2;
 			}
 		}
-		
+
 		// Check diagonals
 		if(	(grid[0][0].value != 0 
 				&& grid[0][0].value == grid[1][1].value 
@@ -63,10 +75,10 @@ public int checkWinner() {
 				|| (grid[0][2].value != 0
 				&& grid[0][2].value == grid[1][1].value 
 				&& grid[1][1].value == grid[2][0].value)){
-					if(mainWindow.xTurn) return 1;
-					return 2;
+			if(mainWindow.xTurn) return 1;
+			return 2;
 		}
-		
+
 		// Check tie
 		// If empty square found return 0, otherwise return 3 signifying tie
 		for(int r = 0; r < 3; r++) {
@@ -76,10 +88,10 @@ public int checkWinner() {
 				}
 			}
 		}
-		
+
 		return 3;
 	}
-	
-	
-	
+
+
+
 }
