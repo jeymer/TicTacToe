e08afef0d78f550672011175a1145a1af2d399c3
Jeffrey <jeymer@live.com>
2017-01-23 13:18:21 -0500

    Removed unused imports

--- Tic Tac Toe/src/tictactoe/LetterWindow.java
+++ Tic Tac Toe/src/tictactoe/LetterWindow.java
@@ -1,10 +1,7 @@
 package tictactoe;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
 import java.awt.GridLayout;
-import java.awt.Panel;
-
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
